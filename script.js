b03eae959195626db2f94016ec95d322a59d525c
jhonan <jhonancalebm@gmail.com>
2022-08-09 14:23:35 -0500

    formulario de send email

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HEADER ESTILOS AL SCROLEAR ABAJO
 var cabeza = $("#nav");
 $(window).scroll(function () {
   if ($(window).scrollTop() > 10) {
@@ -7,6 +8,7 @@ $(window).scroll(function () {
   }
 });
 
+// MOSTRAR BARRA DE MENU
 const btnBar = document.querySelector(".bar");
 let bar = document.querySelector(".menu");
 let btnNav = document.querySelectorAll(".btn-nav");
@@ -15,4 +17,19 @@ function showBar() {
 }
 
 btnBar.onclick = showBar;
-btnNav.forEach(item => item.onclick=showBar)
+btnNav.forEach((item) => (item.onclick = showBar));
+
+// MOSTRAR FORM DE SEND Email
+
+const btnSend = document.querySelector(".send");
+const btnSendButton = document.querySelector("#btn-email");
+const btnSendClose = document.querySelector("#btnCerrar");
+let form = document.querySelector(".boxSend");
+
+function showForm() {
+  form.classList.toggle("hidden");
+}
+
+btnSend.onclick = showForm;
+btnSendButton.onclick = showForm;
+btnSendClose.onclick = showForm;
