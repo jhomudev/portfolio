f6f02b772898dfe38e228de43557e526ffe40248
JhonanCaleb <jhonancalebm@gmail.com>
2023-09-15 09:28:18 -0500

    change to dev react-vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <>
+      <h1 className='text-2xl bg-red-300'>Hola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <NextUIProvider>
+    <App />
+  </NextUIProvider>
+)
